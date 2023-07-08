c319f6eed5ef2552fc68297ced4a09577a9ef721
fvrests <fvrests@icloud.com>
2023-07-08 20:21:49 +0200

    move svg icons to components

--- src/routes/item/[id]/+page.svelte
+++ src/routes/item/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Comment from './comment.svelte';
 	import Capsule from '$lib/capsule.svelte';
+	import { ChatBubbleIcon, UserCircleIcon, ArrowCircleIcon } from '$lib/icons';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -60,20 +61,7 @@
 
 		<div class="mt-6 flex flex-wrap items-center gap-3">
 			<Capsule href="#comments" ignoreVisited>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="h-4 w-4"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
-					/>
-				</svg>
+				<ChatBubbleIcon size={16} />
 
 				<p>
 					{data.comments_count}
@@ -83,40 +71,14 @@
 
 			{#if data.user}
 				<Capsule href="/user/{data.user}">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="h-4 w-4"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-						/>
-					</svg>
+					<UserCircleIcon size={16} />
 
 					<p>{data.user}</p>
 				</Capsule>
 			{/if}
 
 			<Capsule href={replyLink} ignoreVisited>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="h-4 w-4 -rotate-45"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-					/>
-				</svg>
+				<ArrowCircleIcon size={16} />
 
 				<p>Reply</p>
 			</Capsule>
--- src/routes/item/[id]/comment.svelte
+++ src/routes/item/[id]/comment.svelte
@@ -2,6 +2,7 @@
 	export let author;
 	export let parentId;
 	export let comment;
+	import { LinkIcon } from '$lib/icons';
 </script>
 
 {#if !comment.deleted}
@@ -25,21 +26,9 @@
 					<a
 						href="#{comment.id}"
 						class="flex h-6 w-6 items-center justify-center rounded-full bg-surface"
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="h-3.5 w-3.5"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-							/>
-						</svg></a
 					>
+						<LinkIcon size={14} />
+					</a>
 				</div>
 			</summary>
 
