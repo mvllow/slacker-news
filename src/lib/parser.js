9139ede981d577472632669699eff59c67df1afb
mvllow <mvllow@icloud.com>
2023-09-28 17:57:44 -0500

    feat: replace hn site links
    This commit replaces Hacker News item links with the internal
    equivalent, e.g. https://news.mellow.dev/item/xxx

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -3,6 +3,7 @@ function formatContent(content) {
 	let formattedContent = content;
 
 	formattedContent = formatBlockquotes(formattedContent);
+	formattedContent = replaceOriginalLinks(formattedContent);
 
 	return formattedContent;
 }
@@ -16,6 +17,19 @@ function formatBlockquotes(content) {
 	return content.replaceAll(
 		/<p>(<i>)?(&gt;.*?)(<p>)?/gm,
 		"<p class='blockquote'>$1$2",
+	);
+}
+
+/**
+ * Replace links to news.ycombinator.com with links to news.mellow.dev
+ *
+ * @param {string} content
+ **/
+function replaceOriginalLinks(content) {
+	// TODO(test): http://localhost:5173/item/37690996
+	return content.replaceAll(
+		/<a href=".*news\.ycombinator\.com.*item\?id=(.*)">(.*)<\/a>/gm,
+		"<a href='https://news.mellow.dev/item/$1'>$2</a>",
 	);
 }
 
