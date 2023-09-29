ed41bed044488041597b614d4c7d4cd1964f174c
mvllow <mvllow@icloud.com>
2023-09-29 10:10:58 -0500

    feat: improve visual hierchy for comments

--- src/routes/item/[id]/comment.svelte
+++ src/routes/item/[id]/comment.svelte
@@ -18,15 +18,16 @@
 {#if !comment.deleted}
 	<article id={comment.id.toString()} class:isTarget>
 		<details open class="mt-6">
-			<summary class="flex items-center gap-1.5 text-subtle">
-				<div class="flex w-full items-center gap-1.5 text-sm font-medium">
+			<summary class="flex items-center gap-1.5 text-subtle/40">
+				<div class="flex w-full items-center gap-1.5">
 					<a
 						href="/item/{comment.id}?rootId={rootId}"
-						class="font-bold"
-						class:text-text={author !== comment.user}
-						class:text-primary={author === comment.user}
+						class="text-sm decoration-text hover:underline {author ===
+						comment.user
+							? 'font-bold text-primary'
+							: 'font-medium text-subtle'}"
 						>{comment.user}&nbsp;
-						<span class="font-medium text-subtle"
+						<span class="text-xs font-medium text-subtle/60"
 							>{comment.time_ago.replace(/(a|an)\W.*?/, '1 ')}</span
 						></a
 					>
@@ -35,42 +36,32 @@
 
 					<a
 						href="#{comment.id}"
-						class="flex h-6 w-6 items-center justify-center rounded-full bg-surface"
+						class="flex h-6 w-6 items-center justify-center rounded-full text-subtle/60 hover:bg-surface hover:text-text"
 					>
 						<LinkIcon size={14} />
 					</a>
 				</div>
 			</summary>
 
-			<Content content={comment.content} />
+			<div class="ml-1 border-l pl-5" class:isTarget>
+				<Content content={comment.content} />
 
-			{#if comment.comments.length > 0}
-				<ul role="list">
-					{#each comment.comments as child}
-						<li class="border-l pl-6">
-							<svelte:self {author} {rootId} comment={child} />
-						</li>
-					{/each}
-				</ul>
-			{/if}
+				{#if comment.comments.length > 0}
+					<ul role="list">
+						{#each comment.comments as child}
+							<li>
+								<svelte:self {author} {rootId} comment={child} />
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			</div>
 		</details>
 	</article>
 {/if}
 
 <style lang="postcss">
-	.isTarget > details > summary,
-	.isTarget > details > .formatted-content {
-		@apply bg-primary/5;
-	}
-	.isTarget > details > summary {
-		@apply -mx-2 -mt-2 border-t-2 border-primary/50 px-2 pt-2;
-	}
-	.isTarget > details > .formatted-content {
-		@apply -mx-2 -mb-2 px-2 pb-2;
-	}
-	/* HACK: Replace margin with padding on first content element
-	 * to maintain target background */
-	.isTarget > details > .formatted-content :global(> *) {
-		@apply mt-0 pt-3;
+	.isTarget {
+		@apply border-primary/50;
 	}
 </style>
