dfc4b163f4aef3c3bb05e83e5f05a834b8f97f44
not <mvllow@icloud.com>
2023-10-23 12:31:11 -0500

    feat: add user rss feeds (#20)

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -3,4 +3,5 @@ export { default as ChatBubbleIcon } from './chat-bubble.svelte';
 export { default as DocumentTextIcon } from './document-text.svelte';
 export { default as HeartIcon } from './heart.svelte';
 export { default as LinkIcon } from './link.svelte';
+export { default as RssIcon } from './rss.svelte';
 export { default as UserCircleIcon } from './user-circle.svelte';
