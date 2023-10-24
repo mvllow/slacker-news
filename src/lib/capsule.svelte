587e0f9a19fb30dc613c96a4aa6baa851e541d24
not <mvllow@icloud.com>
2023-10-23 20:30:08 -0500

    feat: quick refresh content when outdated or offline (#23)
    This commit adds alternative styles to the capsule component, allowing a
    more uniform design between buttons and clickable banners.

--- src/lib/capsule.svelte
+++ src/lib/capsule.svelte
@@ -2,12 +2,40 @@
 	/** @type string */
 	export let href;
 	export let ignoreVisited = false;
+	export let banner = false;
+	export let primary = false;
 </script>
 
 <a
 	{href}
-	class="inline-flex items-center justify-center gap-1.5 rounded-full bg-surface px-3 py-2 font-mono text-xs font-medium hover:bg-overlay hover:text-text [&_svg]:text-subtle"
+	class="inline-flex items-center justify-center gap-1.5 rounded-full px-3 py-2 [&_svg]:shrink-0"
+	class:banner
+	class:primary
 	class:visited:text-subtle={!ignoreVisited}
 >
 	<slot />
 </a>
+
+<style lang="postcss">
+	:not(.banner) {
+		@apply font-mono text-xs font-medium;
+	}
+	:not(.primary) {
+		@apply bg-surface hover:bg-overlay hover:text-text;
+	}
+	:not(.primary) :global(svg) {
+		@apply text-subtle;
+	}
+	.banner {
+		@apply border text-sm font-medium;
+	}
+	.banner:not(.primary) {
+		@apply border border-muted/10 bg-muted/5 text-text hover:bg-muted/10;
+	}
+	.primary {
+		@apply border-primary/10 bg-primary/5 text-primary hover:bg-primary/10;
+	}
+	.primary :global(svg) {
+		@apply text-primary/80;
+	}
+</style>
